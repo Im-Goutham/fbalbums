8a59d02b08742c2d8348dfaedf627e43b1c289e1
Goutham <goutham@commercepundit.com>
2019-02-15 19:49:12 +0530

    Updates

---
+++ precache-manifest.14c49dcce0e3acb4f0cf73a3deeae89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29256fbec02de5bf9b1",
+    "url": "/fbalbums/static/css/main.bf33c49d.chunk.css"
+  },
+  {
+    "revision": "f29256fbec02de5bf9b1",
+    "url": "/fbalbums/static/js/main.f29256fb.chunk.js"
+  },
+  {
+    "revision": "1c6d626e29305a06834f",
+    "url": "/fbalbums/static/css/1.e5894e97.chunk.css"
+  },
+  {
+    "revision": "1c6d626e29305a06834f",
+    "url": "/fbalbums/static/js/1.1c6d626e.chunk.js"
+  },
+  {
+    "revision": "1a3070fbb8e24aba32cc",
+    "url": "/fbalbums/static/js/runtime~main.1a3070fb.js"
+  },
+  {
+    "revision": "6574aa0ab2fdb345fd12bb07c8280905",
+    "url": "/fbalbums/index.html"
+  }
+];
